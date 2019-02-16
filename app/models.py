41788473dd0cd53eb0aefe8785bfe7a9b11cc18f
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-15 23:05:30 -0700

    Create posts table and relationship

--- app/models.py
+++ app/models.py
@@ -6,7 +6,21 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     avatar = db.Column(db.String(200), nullable=False, default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
 
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    description = db.Column(db.String(120))
+    image_url = db.Column(db.String(120))
+    date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    post_type = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
