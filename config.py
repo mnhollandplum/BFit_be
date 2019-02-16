26e87b85b78bfd532a8571c22424cc8f7c701a59
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-16 10:35:46 -0700

    Fix db url

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = ('DATABASE_URL') or \ 'postgresql://localhost/bfit'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \ 'postgresql://localhost/bfit'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
