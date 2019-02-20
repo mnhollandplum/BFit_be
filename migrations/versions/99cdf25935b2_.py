b08d9b740b83c4956c1539b85db18657cab25871
TimothyFell <timothy.fell846@gmail.com>
2019-02-20 13:12:57 -0700

    Rebase onto new master

--- migrations/versions/99cdf25935b2_.py
+++ migrations/versions/99cdf25935b2_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 55940faf18de
+Revision ID: 99cdf25935b2
 Revises: 
-Create Date: 2019-02-20 11:34:50.836862
+Create Date: 2019-02-20 13:12:17.846624
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '55940faf18de'
+revision = '99cdf25935b2'
 down_revision = None
 branch_labels = None
 depends_on = None
