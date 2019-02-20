3a3b90696426eeca1bf48dd775a132ac7914088a
Nikki Holland-Plum <mnhollandplum@gmail.com>
2019-02-20 14:05:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bfit"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bfit"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
